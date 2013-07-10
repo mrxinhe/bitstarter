ad8e96c2de9e323b69ab274b75d45c59f2927724
Xin HE <dr.xin.he@gmail.com>
2013-07-10 07:09:13 +0000

    web.js updated 2

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World again!');
+  response.send('Hello World Again and Again!');
 });
 
 var port = process.env.PORT || 5000;
